e2be90463746b0bdf929d405d914b1b125ad13ce
unknown <1907402902@qq.com>
2020-08-05 15:37:45 +0800

    m

---
+++ front2/src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import vuex from 'vuex'
+
+Vue.use(vuex)
+
+export default new vuex.Store({
+    state:{
+
+    },
+    getters:{
+
+    },
+    mutations:{
+
+    },
+    actions:{
+
+    },
+    modules:{
+
+    }
+})
--- front2/src/views/home/home.vue
+++ front2/src/views/home/home.vue
@@ -1,12 +1,30 @@
 <template>
     <div>
         <h1>Home....</h1>
+      <ul>
+        <li v-for="(item,index) in list" :key="index">{{item.id}}-{{item.name}}-{{item.age}}-{{item.sex}}</li>
+      </ul>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
     export default {
-        name: "home"
+        name: "home",
+      data(){
+          return{
+            list:[]
+          }
+      },
+      created() {
+        axios.get('http://127.0.0.1:9022/user/demo')
+            .then((data)=>{
+              console.log(data);
+              if(data.data.code===200){
+                this.list = data.data.data.list
+              }
+            })
+      }
     }
 </script>
 
